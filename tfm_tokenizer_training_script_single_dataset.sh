6c4d8203cd66d407364e6cca65abaca062ed3ed1
Jathurshan0330 <jathurshanpradeepkumar@gmail.com>
2026-04-21 19:37:35 -0500

    added all finetuned seed models

--- tfm_tokenizer_training_script_single_dataset.sh
+++ tfm_tokenizer_training_script_single_dataset.sh
@@ -1,5 +1,5 @@
 
-Dataset='TUEV'
+Dataset='CHBMIT'
 random_seed=5
 code_book_size=8192
 emb_size=64
@@ -15,8 +15,8 @@ fi
 ###############################################################################################
 vqvae_pretrained_path="${experiment_path}/TFM_TOKENIZER_${Dataset}_2x2x8_${code_book_size}_${emb_size}/tfm_tokenizer_last.pth"
 mem_pretrained_path="${experiment_path}/Downstream_Model_MASKED_TOKEN_PREDICTION_PRETRAINING_${Dataset}_${code_book_size}_${emb_size}/tfm_encoder_mtp.pth"
-pretrain_gpu='0,1' 
-finetune_gpu1='0'
+pretrain_gpu='5,6' 
+finetune_gpu1='5'
 # tfm-tokenizer tokenizer learning
 python tfm_tokenizer_token_learning.py --dataset_name $Dataset --code_book_size $code_book_size --emb_size $emb_size --resampling_rate 200 --random_seed $random_seed  --gpu $pretrain_gpu --save_path $experiment_path
 # tfm-encoder masked token prediction pretraining
